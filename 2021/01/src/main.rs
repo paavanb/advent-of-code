18b072df7a0bc0a85184d4836a0acf310a2ba356
Paavan Bhavsar <paavan@kensho.com>
2021-12-02 12:30:50 -0500

    Clean up

--- 2021/01/src/main.rs
+++ 2021/01/src/main.rs
@@ -7,27 +7,6 @@ fn main() {
     println!("Part 1b: {}", part_1b());
 }
 
-
-fn get_measurements() -> Vec<i32> {
-    let contents = fs::read_to_string("data/sonar_sweeps.txt").expect("Something went wrong.");
-    let measurements = contents.trim_end().split("\n").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
-
-    return measurements;
-}
-
-fn get_increases(values: Vec<i32>) -> i32 {
-    let (increases, _) = values.iter().fold((0, None), |(increases, prev), item| {
-        if let Some(value) = prev {
-            if value < item {
-                return (increases + 1, Some(item))
-            }
-        }
-        return (increases, Some(item))
-    });
-
-    return increases;
-}
-
 fn part_1a() -> i32 {
     let measurements = get_measurements();
     return get_increases(measurements);
@@ -39,4 +18,32 @@ fn part_1b() -> i32 {
     let windows = measurements.iter().tuple_windows::<(_, _, _)>();
     let sum = windows.map(|(a, b, c)| a + b + c);
     return get_increases(sum.collect_vec());
+}
+
+
+/**
+ * Grab measurements from puzzle input file.
+ */
+fn get_measurements() -> Vec<i32> {
+    let contents = fs::read_to_string("data/sonar_sweeps.txt").expect("Something went wrong.");
+    let measurements = contents.trim_end().split("\n").map(|s| s.parse::<i32>().unwrap()).collect_vec();
+
+    return measurements;
+}
+
+/**
+ * Given a vector of numbers, return the number of times the value increases over two successive
+ * elements.
+ */
+fn get_increases(values: Vec<i32>) -> i32 {
+    let (increases, _) = values.iter().fold((0, None), |(increases, prev), item| {
+        if let Some(value) = prev {
+            if value < item {
+                return (increases + 1, Some(item))
+            }
+        }
+        return (increases, Some(item))
+    });
+
+    return increases;
 }
