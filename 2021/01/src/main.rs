2b2f8bfdf379ffbf6f677ad1f2d93ff20f8ae5de
Paavan Bhavsar <paavan@kensho.com>
2021-12-02 12:27:23 -0500

    Implement 1b

--- 2021/01/src/main.rs
+++ 2021/01/src/main.rs
@@ -1,9 +1,22 @@
 use std::fs;
+use itertools::Itertools;
+
 
 fn main() {
+    println!("Part 1a: {}", part_1a());
+    println!("Part 1b: {}", part_1b());
+}
+
+
+fn get_measurements() -> Vec<i32> {
     let contents = fs::read_to_string("data/sonar_sweeps.txt").expect("Something went wrong.");
-    let measurements = contents.trim_end().split("\n").map(|s| s.parse::<i32>().unwrap());
-    let (increases, _) = measurements.fold((0, None), |(increases, prev), item| {
+    let measurements = contents.trim_end().split("\n").map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>();
+
+    return measurements;
+}
+
+fn get_increases(values: Vec<i32>) -> i32 {
+    let (increases, _) = values.iter().fold((0, None), |(increases, prev), item| {
         if let Some(value) = prev {
             if value < item {
                 return (increases + 1, Some(item))
@@ -12,5 +25,18 @@ fn main() {
         return (increases, Some(item))
     });
 
-    println!("{}", increases);
+    return increases;
+}
+
+fn part_1a() -> i32 {
+    let measurements = get_measurements();
+    return get_increases(measurements);
+}
+
+fn part_1b() -> i32 {
+    let measurements = get_measurements();
+
+    let windows = measurements.iter().tuple_windows::<(_, _, _)>();
+    let sum = windows.map(|(a, b, c)| a + b + c);
+    return get_increases(sum.collect_vec());
 }
